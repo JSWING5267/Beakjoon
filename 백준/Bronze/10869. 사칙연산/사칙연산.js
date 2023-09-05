2e77a97f42c177f6664df697141deee1219394e1
박준서 <101302503+JSWING5267@users.noreply.github.com>
2023-09-05 22:20:10 +0900

    [Bronze V] Title: 사칙연산, Time: 160 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+var input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+//var input = require("fs").readFileSync("example.txt").toString().split(" ");
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
