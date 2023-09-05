bf70dfdf68b035e280f82d9f939fbec61912bb6f
박준서 <101302503+JSWING5267@users.noreply.github.com>
2023-09-05 22:45:57 +0900

    [Bronze V] Title: ??!, Time: 184 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,4 @@
+var input = require("fs").readFileSync("/dev/stdin").toString().trim();
+//var input = require("fs").readFileSync("example.txt").toString().trim();
+var a = input;
+console.log(a + "??!");
