28c9e30bf0fd2a05ad03bc35bad34de55d14c7b4
박준서 <101302503+JSWING5267@users.noreply.github.com>
2023-09-07 19:46:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,4 @@
+var input = require("fs").readFileSync("/dev/stdin").toString().trim();
+//var input = require("fs").readFileSync("example.txt").toString().trim();
+var a = parseInt(input);
+console.log(a - 543);
