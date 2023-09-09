71a97fa607450c7be83f0ce61312831e76f74e26
박준서 <101302503+JSWING5267@users.noreply.github.com>
2023-09-09 16:28:11 +0900

    [Bronze V] Title: 윤년, Time: 136 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,9 @@
+var input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+//var input = require("fs").readFileSync("example.txt").toString().split(" ");
+var a = parseInt(input[0]);
+
+if (a % 4 == 0) {
+  if (a % 100 !== 0 || a % 400 == 0) {
+    console.log("1");
+  } else console.log("0");
+} else console.log("0");
